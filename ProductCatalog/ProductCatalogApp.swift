6ed8a55cf41ae2c8b8444a84fc0945ed79f26bb6
Francisco Casillas <chiccasillas@gmail.com>
2025-02-25 10:41:05 -0600

    Initial Commit

---
+++ ProductCatalog/ProductCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogApp.swift
+//  ProductCatalog
+//
+//  Created by Francisco Jean on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
