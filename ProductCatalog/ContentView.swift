a7949020f98cadcef9a6b90277a1d5b6c601362f
Francisco Casillas <chiccasillas@gmail.com>
2025-02-25 10:50:29 -0600

    Create prouct struct

--- ProductCatalog/ContentView.swift
+++ ProductCatalog/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct Product {
+        var id: UUID
+        var productName: String 
+        var price: Double 
+        var image: String
+        var webImage: String
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0, green: 0.478, blue: 1, alpha: 1)), .white]), 
+                                   startPoint: .top, 
+                                   endPoint: .bottom)
+                        .ignoresSafeArea()
+            VStack {
+                Text("Product Catalog")
+                    . font(.custom( "Montserrat-Bold", size: 25))
+                    .padding(.top, 30)
+                ScrollView {
+                    
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
